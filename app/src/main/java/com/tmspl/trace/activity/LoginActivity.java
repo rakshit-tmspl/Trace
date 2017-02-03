105c4f6c82f19c6f450adf0de0a43612018ae09f
Rakshit <rakshit1993.rs@gmail.com>
2017-02-03 14:41:53 +0530

    Change Login Type.

--- app/src/main/java/com/tmspl/trace/activity/LoginActivity.java
+++ app/src/main/java/com/tmspl/trace/activity/LoginActivity.java
@@ -3,10 +3,13 @@ package com.tmspl.trace.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,14 +19,22 @@ import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 import com.tmspl.trace.R;
+import com.tmspl.trace.activity.homeactivity.HomeActivity;
 import com.tmspl.trace.activity.ridersactivity.RiderHomeActivity;
+import com.tmspl.trace.api.API;
+import com.tmspl.trace.api.RetrofitCallbacks;
+import com.tmspl.trace.apimodel.LoginNewResponse;
+import com.tmspl.trace.extra.Constants;
 import com.tmspl.trace.extra.LocationService;
 import com.tmspl.trace.extra.MyApplication;
+import com.tmspl.trace.extra.Preferences;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity implements Validator.ValidationListener {
 
@@ -44,6 +55,14 @@ public class LoginActivity extends AppCompatActivity implements Validator.Valida
     Validator validator;
     @BindView(R.id.tv_forgot_password)
     TextView tvForgotPassword;
+    @BindView(R.id.rb_user)
+    AppCompatRadioButton rbUser;
+    @BindView(R.id.rb_rider)
+    AppCompatRadioButton rbRider;
+    @BindView(R.id.rg_user_type)
+    RadioGroup rgUserType;
+
+    public static String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +88,18 @@ public class LoginActivity extends AppCompatActivity implements Validator.Valida
             }
         });
 
+
+        rgUserType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.rb_user) {
+                    type = "user";
+                } else {
+                    type = "rider";
+                }
+            }
+        });
+
     }
 
     /**
@@ -79,115 +110,88 @@ public class LoginActivity extends AppCompatActivity implements Validator.Valida
         String email = etUsername.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
-        startService();
-        startActivity(new Intent(LoginActivity.this, RiderHomeActivity.class));
-//        API.getInstance().loginUser(LoginActivity.this, Constants.AUTH, email, password, new RetrofitCallbacks<LoginNewResponse>(LoginActivity.this) {
-//            @Override
-//            public void onResponse(Call<LoginNewResponse> call, Response<LoginNewResponse> response) {
-//                super.onResponse(call, response);
-//
-//                if (response.isSuccessful()) {
-//
-//                    if (response.body() == null) {
-//                        Toast.makeText(LoginActivity.this, "Null", Toast.LENGTH_SHORT).show();
-//                    } else {
-//                        LoginNewResponse.ResponseJsonBean responseJsonBean = response.body().getResponseJson();
-//
-//                        int user = responseJsonBean.getUserUsertype();
-//                        int rider = responseJsonBean.getRiderUsertype();
-////
-//                        Log.e(TAG, "onResponse: " + rider);
-//
-//                        List<LoginNewResponse.ResponseJsonBean.UserQueryResultBean> userList = responseJsonBean
-//                                .getUserQueryResult();
-//
-//                        List<LoginNewResponse.ResponseJsonBean.RiderQueryResultBean> riderList = responseJsonBean
-//                                .getRiderQueryResult();
-//
-//                        Preferences.savePreferences(LoginActivity.this, "usertype", String.valueOf(user));
-//
-//                        if (Constants.ut == 3) {
-//
-//                            Preferences.savePreferences(LoginActivity.this, "usertype", String.valueOf(rider));
-//
-//                            for (LoginNewResponse.ResponseJsonBean.RiderQueryResultBean rider1 : riderList) {
-//                                Log.e(TAG, "onResponse: RIDER_ID " + rider1.getRiderId());
-//                                Preferences.savePreferences(LoginActivity.this, "rider_id", rider1.getRiderId());
-//                                Preferences.savePreferences(LoginActivity.this, "first_name", rider1.getFirstName());
-//                                Preferences.savePreferences(LoginActivity.this, "last_name", rider1.getFirstName());
-//                                Preferences.savePreferences(LoginActivity.this, "mobile", rider1.getMobile());
-//                                Preferences.savePreferences(LoginActivity.this, "email", rider1.getEmail());
-//                                Preferences.savePreferences(LoginActivity.this, "rider_id", rider1.getRiderId());
-//                            }
-//
-//                            Preferences.savePreferences(LoginActivity.this, "usertype", "3");
-//                            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//                            StrictMode.setThreadPolicy(policy);
-////                            if (record.getString("rider_image").equals("noimage.jpg")) {
-////                                logo = BitmapFactory.decodeResource(getResources(), R.drawable.rider_name);
-////                            } else {
-////                                logo = MyImageLoader.getImageFromUrl(context, Constant.Image_IP + record.getString("rider_image"));
-////                            }
-//
-//                            //  Preferences.savePreferences(context, "r_image", Utility.encodeTobase64(logo));
-//
-//
-//                            startService();
-//                        } else {
-//                            for (LoginNewResponse.ResponseJsonBean.UserQueryResultBean user1 : userList) {
-//                                Log.e(TAG, "onResponse: user_id : " + user1.getUserId());
-//                                Preferences.savePreferences(LoginActivity.this, "first_name", user1.getFirstName());
-//                                Preferences.savePreferences(LoginActivity.this, "last_name", user1.getLastName());
-//                                Preferences.savePreferences(LoginActivity.this, "email", user1.getEmail());
-//                                Preferences.savePreferences(LoginActivity.this, "mobile", user1.getMobile());
-//                                Preferences.savePreferences(LoginActivity.this, "user_id", user1.getUserId());
-//                            }
-//                            Preferences.savePreferences(LoginActivity.this, "usertype", "1");
-//                            finish();
-//                        }
-//
-////                        if (user == 1) {
-////                            for (LoginNewResponse.ResponseJsonBean.UserQueryResultBean user1 : userList) {
-////                                Log.e(TAG, "onResponse: user_id : " + user1.getUserId());
-////                                Preferences.savePreferences(LoginActivity.this, "first_name", user1.getFirstName());
-////                                Preferences.savePreferences(LoginActivity.this, "last_name", user1.getLastName());
-////                                Preferences.savePreferences(LoginActivity.this, "email", user1.getEmail());
-////                                Preferences.savePreferences(LoginActivity.this, "mobile", user1.getMobile());
-////                                Preferences.savePreferences(LoginActivity.this, "user_id", user1.getUserId());
-////                            }
-////                            Preferences.savePreferences(LoginActivity.this, "usertype", "1");
-////                            finish();
-////                        } else if (rider == 3) {
-////
-////                        }
-//                        finish();
-//
-//                        Constants.isLogin = 1;
-//                        if (Preferences.getSavedPreferences(LoginActivity.this, "usertype").equals("3") == false) {
-//                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-//                        } else if (Preferences.getSavedPreferences(LoginActivity.this, "usertype").equals("3")) {
-//                            startActivity(new Intent(LoginActivity.this, RiderHomeActivity.class));
-//                        }
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<LoginNewResponse> call, Throwable t) {
-//                super.onFailure(call, t);
-//
-//                Log.i(TAG, t.toString());
-//                Log.i(TAG, call.toString());
-//                Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+
+        //Login Api
+        API.getInstance().loginUser(LoginActivity.this, Constants.AUTH, email, password, new RetrofitCallbacks<LoginNewResponse>(LoginActivity.this) {
+            @Override
+            public void onResponse(Call<LoginNewResponse> call, Response<LoginNewResponse> response) {
+                super.onResponse(call, response);
+
+                if (response.isSuccessful()) {
+
+                    if (response.body() == null) {
+                        Toast.makeText(LoginActivity.this, "Null", Toast.LENGTH_SHORT).show();
+                    } else {
+                        LoginNewResponse.ResponseJsonBean responseJsonBean = response.body().getResponseJson();
+
+                        int user = responseJsonBean.getUserUsertype();
+                        int rider = responseJsonBean.getRiderUsertype();
+
+                        List<LoginNewResponse.ResponseJsonBean.UserQueryResultBean> userList = responseJsonBean
+                                .getUserQueryResult();
+
+                        List<LoginNewResponse.ResponseJsonBean.RiderQueryResultBean> riderList = responseJsonBean
+                                .getRiderQueryResult();
+
+                        if (type.equals("user")) {
+
+                            //set USER Data and go to User side Activity (HomeActivity)
+                            Preferences.savePreferences(LoginActivity.this, "usertype", String.valueOf(user));
+
+                            //getting user data from UserQueryResultBean list
+
+                            for (LoginNewResponse.ResponseJsonBean.UserQueryResultBean userBean : userList) {
+                                Log.e(TAG, "onResponse: USER-ID" + userBean.getUserId());
+
+                                Preferences.savePreferences(LoginActivity.this, "first_name", userBean.getFirstName());
+                                Preferences.savePreferences(LoginActivity.this, "last_name", userBean.getLastName());
+                                Preferences.savePreferences(LoginActivity.this, "email", userBean.getEmail());
+                                Preferences.savePreferences(LoginActivity.this, "mobile", userBean.getMobile());
+                                Preferences.savePreferences(LoginActivity.this, "user_id", userBean.getUserId());
+                            }
+
+                            finish();
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        } else {
+
+                            //set RIDER Data and go to Rider side Activity (RiderHomeActivity)
+                            Preferences.savePreferences(LoginActivity.this, "usertype", String.valueOf(rider));
+
+                            //Getting rider data from RiderQueryResultBean list
+                            for (LoginNewResponse.ResponseJsonBean.RiderQueryResultBean riderBean : riderList) {
+                                Log.e(TAG, "onResponse: RIDER-ID" + riderBean.getRiderId());
+
+                                Preferences.savePreferences(LoginActivity.this, "first_name", riderBean.getFirstName());
+                                Preferences.savePreferences(LoginActivity.this, "last_name", riderBean.getLastName());
+                                Preferences.savePreferences(LoginActivity.this, "mobile", riderBean.getMobile());
+                                Preferences.savePreferences(LoginActivity.this, "email", riderBean.getEmail());
+                                Preferences.savePreferences(LoginActivity.this, "rider_id", riderBean.getRiderId());
+                            }
+
+                            startService();
+                            finish();
+                            startActivity(new Intent(LoginActivity.this, RiderHomeActivity.class));
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginNewResponse> call, Throwable t) {
+                super.onFailure(call, t);
+
+                Log.i(TAG, t.toString());
+                Log.i(TAG, call.toString());
+                Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     /**
      * Called when one or several {@link Rule}s fail.
      *
      * @param errors List containing references to the {@link View}s and
-     *               {@link Rule}s that failed.
+     * {@link Rule}s that failed.
      */
     @Override
     public void onValidationFailed(List<ValidationError> errors) {
